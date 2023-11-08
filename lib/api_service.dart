faf655fa4656fc1afa569090abcaa81dcb38a5c4
Tiago Rafael <tiagorafael777@gmail.com>
2023-11-08 19:16:45 -0400

    feat: add api

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+class ApiService {
+  Future<List<dynamic>> Posts() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Falha ao carregar os posts');
+    }
+  }
+}
